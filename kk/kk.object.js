cd4bc00b5869163fc93802b9adbe6d6f7e6b183b
zhang hailong <hailongz@qq.com>
2017-02-24 11:09:18 +0800

    Add

---
+++ kk/kk.object.js
@@ -0,0 +1,100 @@
+
+/**
+ * 基类
+ */
+(function(kk){
+	
+	kk.Object = function(){};
+	kk.Object.prototype.init = function() { return this; };
+	
+	kk.get = function(object,key) {
+		
+		var keys;
+		if(key instanceof Array) {
+			keys = key;
+		}
+		else if(key == "" || key == "_") {
+			return object;
+		}
+		else {
+			keys = key.split(".");
+		}
+		
+		var v = object;
+		
+		while(keys.length >0 ){
+			key = keys.shift();
+			if(typeof v == 'object') {
+				v = v[key];
+			}
+			else {
+				return;
+			}
+		}
+		
+		return v;
+	};
+	
+	kk.set = function(object,key,value) {
+		
+		var keys;
+		if(key instanceof Array) {
+			keys = key;
+		}
+		else {
+			keys = key.split(".");
+		}
+		
+		var v = object;
+		
+		while(keys.length >0 ){
+			
+			key = keys.shift();
+			
+			if(keys.length == 0) {
+				v[key] = value;
+			}
+			else {
+				var vv = v[key];
+				if(typeof vv != 'object') {
+					vv = {};
+					v[key] = vv;
+				}
+				v = vv;
+			}
+			
+		}
+	};
+	
+	kk.remove = function(object,key) {
+		
+		var keys;
+		if(key instanceof Array) {
+			keys = key;
+		}
+		else {
+			keys = key.split(".");
+		}
+		
+		var v = object;
+		
+		while(keys.length >0 ){
+			
+			key = keys.shift();
+			
+			if(keys.length == 0) {
+				delete v[key];
+			}
+			else {
+				var vv = v[key];
+				if(typeof vv != 'object') {
+					return;
+				}
+				v = vv;
+			}
+			
+		}
+		
+	};
+	
+})(kk);
---
+++ kk/kk.observer.js
@@ -0,0 +1,155 @@
+
+/**
+ * 事件
+ */
+(function(kk){
+	
+	var KeyObserver = function(){};
+
+	KeyObserver.alloc = function() {
+		var v = new KeyObserver();
+		return v.init.apply(v,arguments);
+	};
+
+	KeyObserver.prototype = new kk.Object();
+
+	KeyObserver.prototype.init = function() {
+		kk.Object.prototype.init.apply(this,arguments);
+		this._fns = [];
+		this._observers = {};
+		return this;
+	};
+
+	KeyObserver.prototype.keyObserver = function(key) {
+		var v = this._observers[key];
+		if(v === undefined) {
+			v = KeyObserver.alloc();
+			this._observers[key] = v; 
+		}
+		return v;
+	};
+
+	KeyObserver.prototype.on = function(keys,i,fn) {
+		if(i < keys.length) {
+			var key = keys[i];
+			var v = this.keyObserver(key);
+			v.on(keys,i + 1, fn);
+		}
+		else {
+			this._fns.push(fn);
+		}
+	};
+
+	KeyObserver.prototype.off = function(keys,i,fn) {
+		if(i < keys.length) {
+			var key = keys[i];
+			var v = this._observers[key];
+			if(v !== undefined) {
+				v.off(keys,i + 1,fn);
+			}
+		}
+		else if(fn === undefined) {
+			this._fns = [];
+		}
+		else {
+			var fns = [];
+			for(var n=0;n<this._fns.length;n++) {
+				var f = this._fns[n];
+				if(f != fn) {
+					fns.push(f);
+				}
+			}
+			this._fns = fns;
+		}
+	};
+
+	KeyObserver.prototype.change = function(keys,i) {
+
+		if(keys === undefined) {
+			var fns = this._fns.slice();
+			for(var n=0;n<fns.length;n++) {
+				var fn = fns[n];
+				fn.apply(null,arguments)
+			}
+			for(var key in this._observers) {
+				var v = this._observers[key];
+				v.change.apply(v,arguments);
+			}
+		}
+		else if(i < keys.length) {
+			var key = keys[i];
+			var fns = [];
+			for(var n=0;n<this._fns.length;n++) {
+				var fn = this._fns[n];
+				if(fn.bubble) {
+					fns.push(fn);
+				}
+			}
+			for(var n=0;n<fns.length;n++) {
+				var fn = fns[n];
+				fn.apply(null,arguments)
+			}
+			var v = this._observers[key];
+			if(v !== undefined) {
+				v.change.apply(v,[keys,i + 1].concat(arguments.slice(2)));
+			}
+		}
+		else {
+			var fns = this._fns.slice();
+			for(var n=0;n<fns.length;n++) {
+				var fn = fns[n];
+				fn.apply(null,arguments)
+			}
+		}
+
+	};
+
+	kk.Observer = function(){};
+	
+	kk.Observer.alloc = function() {
+		var v = new kk.Observer();
+		return v.init.apply(v,arguments);
+	};
+
+	kk.Observer.prototype = new kk.Object();
+	
+	kk.Observer.prototype.init = function(object) {
+		kk.Object.prototype.init.apply(this,arguments);
+		this.key = KeyObserver.alloc();
+		this.object = object;
+		return this;
+	};
+	
+	kk.Observer.prototype.get = function(keys) {
+		if(keys === undefined) {
+			return this.object;
+		}
+		return kk.get(this.object,keys);
+	};
+
+	kk.Observer.prototype.set = function(keys,value) {
+		if(keys === undefined || keys == '_') {
+			this.object = value;
+		}
+		else {
+			if(typeof this.object != "object")
+				this.object = {};
+			}
+			kk.set(this.object,keys,value);
+		}
+		this.change(keys);
+	};
+
+	kk.Observer.prototype.change = function(keys) {
+		if(keys === undefined || keys == '_') {
+			this.key.change();
+		}
+		else if(keys instanceof Array) {
+			this.key.change(keys,0,this);
+		}
+		else if(typeof keys == 'string') {
+			this.key.change(keys.split('.'),0,this);
+		}
+	};
+	
+})(kk);
